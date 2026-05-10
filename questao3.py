839092a43e0c2d5fbfe4a7cea5cab98b5fb58f54
Miguelvin07 <miguelvini121-@gmail.com>
2026-05-10 20:33:27 -0300

    q3 feita

--- questao3.py
+++ questao3.py
@@ -1,2 +1,20 @@
 
-    
+# Leia uma palavra (sem espaços) e descubra qual é o primeiro caractere que aparece duas ou mais vezes nela.
+# Caso não exista nenhum caractere, imprima Nenhum caractere.
+
+# Restrições: não use listas, dicionários, nem .count().
+
+palavra = input("Digite a palavra: ")
+
+for letra in palavra:
+    contador = 0
+    for i in palavra:
+        if i == letra:
+            contador +=1
+    if contador >=2:
+        print(f"A primeira letra que se repete 2 ou mais vezes é: {letra}, se repete {contador} vezes")
+        break
+else:
+    print("Não existe caracteres repetidos")
+
+
